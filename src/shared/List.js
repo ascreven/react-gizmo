3354e6c91563aa8b6b3cb8a378503f8ad581896c
Alexandra Screven <alexandra.screven@gsa.gov>
2020-12-22 15:53:18 -0700

    added mock movies and added them to list

--- src/shared/List.js
+++ src/shared/List.js
@@ -1,13 +1,18 @@
 import "./List.css";
 
-function List() {
+function List(props) {
     return (
         <div className="list">
             <ol className="list__container">
-                <li className="list__item list__group">list group name</li>
-                <li className="list__item">list item 1</li>
+                <li className="list__item list__group">{props.title}</li>
+                {props.items.map(item => (
+                    <li className="list__item" key={item.id}>
+                        {item.title}
+                    </li>
+                ))}
+                {/* <li className="list__item">list item 1</li>
                 <li className="list__item">list item 2</li>
-                <li className="list__item">list item 3</li>
+                <li className="list__item">list item 3</li> */}
             </ol>
         </div>
     );
